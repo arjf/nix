d91be217fac92b9079a538a0643a5b027b45c873
arjf <arjf@protonmail.com>
2026-05-27 14:01:10 +0530

    add:(flake) add zen-browser input; swap nixpkgs to unstable

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Lament System flake";
 
   inputs = {
-    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
-    home-manager= {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-lts.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -12,18 +12,26 @@
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-
-  outputs = { self, nixpkgs, ... } @ inputs:
-  let
-    hostname = "lament";
-  in
-  {
-    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
-      ];
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
     };
   };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      hostname = "lament";
+    in
+    {
+      nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
 }
