fc0082a5f85eac4d226f1f1b0fa854746b5f50a0
arjf <arjf@protonmail.com>
2026-06-15 03:21:24 +0530

    refactor:(hw) replace msi-ec derivation with overrideAttrs

--- modules/hardware/msi.nix
+++ modules/hardware/msi.nix
@@ -10,9 +10,19 @@
 
   # This is a hacky way of running a newer version of msi-ec which supports my fw
   boot.extraModulePackages = [
-    (pkgs.callPackage ../derivations/msi-ec.nix {
-      kernel = config.boot.kernelPackages.kernel;
-    })
+    (config.boot.kernelPackages.msi-ec.overrideAttrs (oldAttrs: {
+      src = pkgs.fetchFromGitHub {
+        owner = "BeardOverflow";
+        repo = "msi-ec";
+        rev = "ffb36db8ae28a520dd570f56735de49845106e0e";
+        sha256 = "sha256-MdFue0buh/8yE4lIdEbLa11pkwfRFvQ6VIU9mZM3hDo=";
+      };
+      patches = [ ];
+      postPatch = ''
+        sed -i 's|/lib/modules/[^/]*/build|$(KERNELDIR)|g' Makefile
+        echo -e '\nmodules_install:\n\t$(MAKE) -C $(KERNELDIR) M=$(CURDIR) modules_install' >> Makefile
+      '';
+    }))
   ];
 
   environment.systemPackages = with pkgs; [
