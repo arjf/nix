c626734fa2c94fb1eded44cbf76c3f2b27553a09
arjf <arjf@protonmail.com>
2026-06-15 03:20:07 +0530

    add:(core) snapper snapshots and nix auto gc

---
+++ modules/core/snapshots.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }: {
+  services.snapper = {
+    configs = {
+      root = {
+        SUBVOLUME = "/";
+        ALLOW_USERS = [ "jo" ];
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+        TIMELINE_LIMIT_HOURLY = 5;
+        TIMELINE_LIMIT_DAILY = 7;
+        TIMELINE_LIMIT_WEEKLY = 4;
+        TIMELINE_LIMIT_MONTHLY = 6;
+        TIMELINE_LIMIT_QUARTERLY = 0;
+        TIMELINE_LIMIT_YEARLY = 0;
+      };
+    };
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+}
