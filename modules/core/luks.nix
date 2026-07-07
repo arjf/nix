741c35a94a8aaa7c1360038623443613dcaf3ed2
arjf <arjf@protonmail.com>
2026-07-07 23:29:52 +0530

    fix:(lvm/luks) migrate boot.initrd.lvm/luks to current nixpkgs API fix:(lament) drop deprecated autoUnlock.tpm2

--- modules/core/luks.nix
+++ modules/core/luks.nix
@@ -6,10 +6,10 @@
 }:
 {
   boot.initrd.systemd.enable = true;
+  boot.initrd.systemd.tpm2.enable = true;
 
   boot.initrd.luks.devices."cryptroot" = lib.mkDefault {
     device = "/dev/disk/by-uuid/CHANGEME";
-    autoUnlock.tpm2.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
--- modules/core/lvm.nix
+++ modules/core/lvm.nix
@@ -5,15 +5,12 @@
   ...
 }:
 {
-  boot.initrd.lvm.enable = true;
+  services.lvm.enable = true;
+  boot.initrd.services.lvm.enable = true;
 
   boot.initrd.kernelModules = [
     "dm-snapshot"
     "dm-raid"
     "dm-cache-default"
-  ];
-
-  environment.systemPackages = with pkgs; [
-    lvm2
   ];
 }
