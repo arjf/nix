e85ed1dde19244b44199ff1165b878685686e5de
arjf <arjf@protonmail.com>
2026-07-07 23:30:03 +0530

    add:(discord) switch equicord to vencord with OpenAsar, krisp, Windows-synced plugins

--- modules/user/discord.nix
+++ modules/user/discord.nix
@@ -12,44 +12,205 @@
     enable = true;
 
     discord = {
-      vencord.enable = false; # mutually exclusive with equicord
-      equicord.enable = true;
+      vencord.enable = true;
+      equicord.enable = false;
+      openASAR.enable = true;
       krisp.enable = true;
+
+      settings = {
+        IS_MAXIMIZED = true;
+        BACKGROUND_COLOR = "#000000";
+        audioSubsystem = "experimental";
+        offloadAdmControls = true;
+        DESKTOP_TTI_REMOVE_V8_CACHE_CLEAR = true;
+        DESKTOP_TTI_DNSTCP_WARMUP = true;
+        DESKTOP_TTI_HTTP_CDT = true;
+        DESKTOP_TTI_EARLY_UPDATE_CHECK = true;
+        DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
+        openasar = {
+          setup = true;
+          quickstart = true;
+          multiInstance = true;
+        };
+        asyncVideoInputDeviceInit = true;
+      };
     };
 
     legcord = {
       enable = true;
-      equicord.enable = true;
+      vencord.enable = true;
       settings = {
         SKIP_HOST_UPDATE = true;
         channel = "stable";
         tray = "dynamic";
         minimizeToTray = true;
-        mods = [ "equicord" ];
+        mods = [ "vencord" ];
         doneSetup = true;
       };
     };
 
     config = {
       autoUpdate = true;
-      frameless = true;
-      plugins = {
-        clearUrls.enable = true; # Strips tracking params from shared URLs
-        fixImagesQuality.enable = true; # Loads images at native resolution
-        gameActivityToggle.enable = true; # Toggle game activity button
-        crashHandler.enable = true; # Recovers from crashes without restart
-        messageLatency.enable = true; # Shows ⚠ indicator on slow-to-send messages
-        gifPaste.enable = true; # Gif picker inserts link instead of instant-send
-        noReplyMention.enable = true; # Disables reply pings by default
-        # silentTyping.enable = true; # Vencord-only, not in Equicord
-        # spotifyControls.enable = true; # Vencord-only, not in Equicord
-        imageZoom.enable = true; # Scroll-wheel zoom on images
-        betterFolders.enable = true; # Folder sidebar + folder management
-        noDevtoolsWarning.enable = true; # Removes "HOLD UP" console banner
+      autoUpdateNotification = true;
+      useQuickCss = true;
+      frameless = false;
+      transparent = true;
+      disableMinSize = true;
 
-        # Bannable
-        # fakeNitro.enable = true; # sends emojis/stickers without Nitro,
-        messageLogger.enable = true; # logs deleted & edited messages server-side.
+      plugins = {
+        alwaysAnimate.enable = true;
+        alwaysExpandRoles.enable = true;
+        alwaysTrust = {
+          enable = true;
+          domain = true;
+          file = true;
+        };
+        anonymiseFileNames.enable = true;
+        betterFolders = {
+          enable = true;
+          sidebar = true;
+          showFolderIcon = 1;
+          keepIcons = false;
+          closeAllHomeButton = false;
+          closeAllFolders = false;
+          forceOpen = false;
+          sidebarAnim = true;
+        };
+        betterGifAltText.enable = true;
+        betterGifPicker.enable = true;
+        betterRoleContext.enable = true;
+        betterRoleDot = {
+          enable = true;
+          bothStyles = false;
+          copyRoleColorInProfilePopout = false;
+        };
+        betterSessions = {
+          enable = true;
+          backgroundCheck = false;
+        };
+        betterSettings = {
+          enable = true;
+          disableFade = true;
+          eagerLoad = true;
+          organizeMenu = true;
+        };
+        betterUploadButton.enable = true;
+        biggerStreamPreview.enable = true;
+        callTimer.enable = true;
+        characterCounter.enable = true;
+        clearUrls.enable = true;
+        copyStickerLinks.enable = true;
+        copyUserUrls.enable = true;
+        crashHandler.enable = true;
+        customRpc.enable = true;
+        fixImagesQuality = {
+          enable = true;
+          originalImagesInChat = false;
+        };
+        fixSpotifyEmbeds.enable = true;
+        fixYoutubeEmbeds.enable = true;
+        forceOwnerCrown.enable = true;
+        gameActivityToggle = {
+          enable = true;
+          location = "PANEL";
+          oldIcon = false;
+        };
+        gifPaste.enable = true;
+        iLoveSpam.enable = true;
+        imageFilename = {
+          enable = true;
+          showFullUrl = false;
+        };
+        ircColors = {
+          enable = true;
+          memberListColors = true;
+          lightness = 70;
+          applyColorOnlyInDms = false;
+          applyColorOnlyToUsersWithoutColor = false;
+        };
+        keepCurrentChannel.enable = true;
+        messageLinkEmbeds.enable = true;
+        moreQuickReactions = {
+          enable = true;
+          reactionCount = 5;
+        };
+        noOnboardingDelay.enable = true;
+        noSystemBadge.enable = true;
+        notificationVolume = {
+          enable = true;
+          notificationVolume = 100.0;
+        };
+        oneko.enable = true;
+        permissionsViewer.enable = true;
+        petpet.enable = true;
+        pictureInPicture.enable = true;
+        platformIndicators = {
+          enable = true;
+          colorMobileIndicator = true;
+          list = true;
+          badges = true;
+          messages = true;
+        };
+        previewMessage.enable = true;
+        quickMention.enable = true;
+        quickReply.enable = true;
+        replyTimestamp.enable = true;
+        revealAllSpoilers.enable = true;
+        reverseImageSearch.enable = true;
+        sendTimestamps.enable = true;
+        serverInfo.enable = true;
+        serverListIndicators = {
+          enable = true;
+          mode = 2;
+        };
+        showMeYourName = {
+          enable = true;
+          mode = "user-nick";
+          friendNicknames = "dms";
+          displayNames = false;
+          inReplies = false;
+        };
+        sortFriendRequests = {
+          enable = true;
+          showDates = false;
+        };
+        spotifyControls = {
+          enable = true;
+          hoverControls = false;
+        };
+        translate = {
+          enable = true;
+          autoTranslate = false;
+        };
+        typingIndicator = {
+          enable = true;
+          includeMutedChannels = false;
+          includeCurrentChannel = true;
+        };
+        typingTweaks = {
+          enable = true;
+          alternativeFormatting = true;
+        };
+        unindent.enable = true;
+        unlockedAvatarZoom.enable = true;
+        userMessagesPronouns = {
+          enable = true;
+          showSelf = true;
+          pronounsFormat = "LOWERCASE";
+        };
+        userVoiceShow = {
+          enable = true;
+          showInUserProfileModal = true;
+          showInMemberList = true;
+          showInMessages = true;
+        };
+        validReply.enable = true;
+        validUser.enable = true;
+        voiceDownload.enable = true;
+        voiceMessages.enable = true;
+        volumeBooster.enable = true;
+        whoReacted.enable = true;
+        youtubeAdblock.enable = true;
       };
     };
   };
