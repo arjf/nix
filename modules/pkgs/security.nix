4a5775276e91abcc333b60be1e1b8703c5d38e50
arjf <arjf@protonmail.com>
2026-08-01 17:49:50 +0530

    add:(pkg) bitwarden-deskop electron fix has been merged upstream and is no longer insecure. Re-added.

--- modules/pkgs/security.nix
+++ modules/pkgs/security.nix
@@ -11,7 +11,7 @@
   environment.systemPackages = with pkgs; [
     gnupg
     pinentry-curses
-    # bitwarden-desktop # waiting on upstream fix on electron ver
+    bitwarden-desktop
     bitwarden-cli
     keepassxc
     keybase-gui
